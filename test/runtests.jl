881c0b83223f63406056bc45db62adde0cab5d4a
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-07-11 14:08:44 +0100

    Add of source file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,15 @@ using Shelve
 using Test
 
 @testset "Shelve.jl" begin
-    # Write your tests here.
+    test_Shelve = Shelve.open("test.lop")
+    @test test_Shelve.data == Dict()
+    test_Shelve["hello"] = "world"
+    @test test_Shelve["hello"] == "world"
+    test_Shelve["complex_data"] = Dict(["a" => "1", "b" => "2"])
+    @test test_Shelve["complex_data"] == Dict(["a" => "1", "b" => "2"])
+
+    # After Insertion
+    full_Shelve = Shelve.open("test.lop")
+    @test full_Shelve["hello"] == "world"
+    @test full_Shelve["complex_data"] == Dict(["a" => "1", "b" => "2"])
 end
