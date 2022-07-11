b6d1faa551ab882ac55a1d08ea1630ca0236ab1d
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-07-11 18:42:51 +0100

    Added key verification in the shelf

--- src/LopShelve.jl
+++ src/LopShelve.jl
@@ -6,17 +6,36 @@ using Serialization: serialize, deserialize
 mutable struct Shelf
     data::Dict{Any,Any}
     filename::String
+    keys::Vector{Any}
+    function Shelf(data::Dict{Any,Any}, filename::String)
+        this = new(data, filename, keys(data) |> collect)
+    end
 end
-Base.show(io::IO, x::Shelf) = print(io, "$(x.data)")
+
+
+Base.show(io::IO, x::Shelf) = print(io, "Shelf:$(x.data)")
+
 function Base.getindex(s::Shelf, key::String)
     return s.data[key]
 end
+
 function Base.setindex!(s::Shelf, value::Any, key::Any)
     s.data[key] = value
+    s.keys = keys(s.data)
     save(s)
 end
 
+function Base.in(item::Any, x::Shelf)
+    return item in keys(x.data)
+end
 
+function Base.keys(x::Shelf)
+    keys(x.data) |> collect
+end
+
+function Base.iterate(x::Shelf, state::Int64=1)::Union{Tuple{Any,Int64}, Nothing}
+    return state > (x.keys |> length) ? nothing : (x.keys[state], state + 1)
+end
 """
     open(filename::String)
 Open a given file in a Shelf object
@@ -34,6 +53,8 @@ function open!(filename::String)
     end
 end
 
+
+
 """
     save(s::Shelf)
 Save the shelve data
@@ -42,6 +63,7 @@ Save the shelve data
 function save(s::Shelf)
     serialize(s.filename, s.data)
 end
+
 
 
 end
