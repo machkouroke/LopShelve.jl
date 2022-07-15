d95efb14c69b62c65656430d5f823ca804f47bff
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-07-15 22:33:19 +0100

    Renaming of makes.jl->make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,5 +3,6 @@ using LopShelve: Shelf, AbstractShelf
 using LopShelve
 makedocs(sitename="LopShelve", modules = [LopShelve])
 deploydocs(
-    repo = "github.com/machkouroke/LopShelve.jl.git"
+    ;repo = "github.com/machkouroke/LopShelve.jl.git", 
+    versions = nothing
 )
