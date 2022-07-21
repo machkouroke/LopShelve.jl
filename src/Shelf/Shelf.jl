95fb4ccc072f294938bc15603594baccb5596b64
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-07-22 00:41:29 +0100

    Add of the open! with do syntax for shelf

--- src/Shelf/Shelf.jl
+++ src/Shelf/Shelf.jl
@@ -25,13 +25,38 @@ function open!(filename::String)
         return Shelf(Dict(), filename)
     end
 end
+"""
+    open(f, filename::String)
+Open a given file in a context manager
+
+If the file don't exist an empty Shelve object is created. Also 
+the file name must'nt have extension
+
+"""
+function open!(func, filename::String)
+    data = open!(filename)
+    try
+        func(data)
+    finally
+        close(data)
+    end
+end
 
 """
     save(s::Shelf)
-Save the shelve data
+Save the shelve data in the file
 
 """
 function save(s::Shelf)
     serialize(s.filename, s.data)
 end
 
+"""
+    close(s::Shelf)
+Close the shelve and save the data in the file and delete the object
+
+"""
+function close(s::Shelf)
+    save(s)
+    s = nothing
+end
